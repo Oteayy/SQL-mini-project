80c54a3924de5f1e9c0a836a5cb16f319ae9cb3f
Oteayy <doroteasopic3@gmail.com>
2025-08-24 17:50:47 +0200

    summary

--- student_project/db.py
+++ student_project/db.py
@@ -1,21 +1,23 @@
 import sqlite3
 from models import Student
 
-sql_create_table = '''
+sql_create_table = """
 CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     surname TEXT NOT NULL,
     birth_year INTEGER
 )
-'''
+"""
+
 def create_table():
-    conn = sqlite3.connect('students.sqlite3')
+    conn = sqlite3.connect("students.sqlite")
     cursor = conn.cursor()
     cursor.execute(sql_create_table)
     conn.commit()
     conn.close()
-    
+
+
 insert_sql = "INSERT INTO students (name, surname, birth_year) VALUES (?, ?, ?)"
 
 def insert_student(student: Student):
@@ -25,6 +27,7 @@ def insert_student(student: Student):
     conn.commit()
     conn.close()
 
+
 get_sql = "SELECT * FROM students"
 
 def get_students():
@@ -33,18 +36,15 @@ def get_students():
     cursor.execute(get_sql)
     rows = cursor.fetchall()
     conn.close()
-    
-    return [Student(row[1], row[2], row[3]) for row in rows]
+    return rows
 
 
 get_sql_by_id = "SELECT * FROM students WHERE id = ?"
 
-def get_student_by_id(id):
+def get_student_by_id(student_id: int):
     conn = sqlite3.connect("students.sqlite")
     cursor = conn.cursor()
-    cursor.execute(get_sql_by_id, (id,))
+    cursor.execute(get_sql_by_id, (student_id,))
     row = cursor.fetchone()
     conn.close()
-    if row:
-        return Student(row[1], row[2], row[3])
-    return None
+    return row
